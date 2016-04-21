6afd7258ce2fca827aac2e35e22f38f9f7300f21
WilliBG18 <b-gray.1@onu.edu>
2016-04-21 17:22:54 -0400

    SampleFIP
    No test or meaningful code. Very little structured code.

---
+++ SampleFIP/Portfolio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFIP
+{
+    public abstract class Portfolio
+    {
+        public abstract void AcctBalance(int balance);
+    }
+    public class ClientAcct : Portfolio
+    {
+        public override void AcctBalance(int balance)
+        {
+
+        }
+    }
+}
---
+++ UnitTestSampleFIP/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleFIP;
+
+namespace UnitTestSampleFIP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestClientAcct()
+        {
+            Portfolio clientAcct = new ClientAcct();
+
+        }
+    }
+}
