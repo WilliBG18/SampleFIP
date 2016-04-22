39aaef4517860464b127d65fc3dce1d6de070aa7
WilliBG18 <b-gray.1@onu.edu>
2016-04-21 23:32:54 -0400

    Started test methods for Stock Portfolio
    empty code, failing test for stkportfolio

--- SampleFIP/StkPortfolio.cs
+++ SampleFIP/StkPortfolio.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace SampleFIP
 {
-    class StkPortfolio
+    public abstract class StkPortfolio
     {
+        public abstract int RandVal(double minPerc, double maxPerc);
+        public abstract double ShowDayChange(double dayChange, double percChange);
+        public abstract double ShowTtlChange(double ttlChange, double dayChange);
+        public abstract double CurrStockVal(double stockVal);
+    }
+    public class Google : StkPortfolio
+    {
+        public override int RandVal(double minPerc, double maxPerc)
+        {
+            throw new NotImplementedException();
+        }
+        public override double ShowDayChange(double dayChange, double percChange)
+        {
+            throw new NotImplementedException();
+        }
+        public override double ShowTtlChange(double ttlChange, double dayChange)
+        {
+            throw new NotImplementedException();
+        }
+        public override double CurrStockVal(double currVal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Gold : StkPortfolio
+    {
+        public override int RandVal(double minPerc, double maxPerc)
+        {
+            throw new NotImplementedException();
+        }
+        public override double ShowDayChange(double dayChange, double percChange)
+        {
+            throw new NotImplementedException();
+        }
+        public override double ShowTtlChange(double ttlChange, double dayChange)
+        {
+            throw new NotImplementedException();
+        }
+        public override double CurrStockVal(double currVal)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- UnitTestSampleFIP/UnitTests.cs
+++ UnitTestSampleFIP/UnitTests.cs
@@ -11,7 +11,7 @@ namespace UnitTestSampleFIP
         public void TestClientAcct()
         {
             Portfolio clientAcct = new ClientAcct();
-           
+
             clientAcct.AcctBalance(1000);
             clientAcct.GetSalary(10000);
             clientAcct.PercInvest(10000, .10);
@@ -56,6 +56,24 @@ namespace UnitTestSampleFIP
             Portfolio clientAcct = new ClientAcct();
             clientAcct.PercInvest(50000, .05);
             Assert.AreEqual(2500, clientAcct.PercInvest(50000, .05));
+        }
+    }
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestGoogle()
+        {
+            StkPortfolio stkPortfolio = new Google();
+
+            stkPortfolio.CurrStockVal(150.00);
+            stkPortfolio.RandVal(.01, .05);
+            stkPortfolio.ShowDayChange(157.5, .05);
+            stkPortfolio.ShowTtlChange(162.23, 157.5);
+            Assert.AreEqual(150.00, stkPortfolio.CurrStockVal(150.00));
+            Assert.AreEqual(.03, stkPortfolio.RandVal(.01, .05));
+            Assert.AreEqual(24.45, stkPortfolio.ShowDayChange(24.45, .05));
+            Assert.AreEqual(150, stkPortfolio.ShowTtlChange(162.23, 157.5));
         }
     }
 }
