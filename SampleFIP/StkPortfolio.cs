4737f7c92fcc6389b05ae15cf83d094815b01d84
WilliBG18 <b-gray.1@onu.edu>
2016-04-24 19:20:38 -0400

    All Tests passed and current refactoring done
    Ready to create forms

--- SampleFIP/StkPortfolio.cs
+++ SampleFIP/StkPortfolio.cs
@@ -8,47 +8,86 @@ namespace SampleFIP
 {
     public abstract class StkPortfolio
     {
-        public abstract int RandVal(double minPerc, double maxPerc);
-        public abstract double ShowDayChange(double dayChange, double percChange);
-        public abstract double ShowTtlChange(double ttlChange, double dayChange);
+        public abstract double RandStkVal(double minPerc, double maxPerc);
+        public abstract int    RandVal(int minPerc, int maxPerc);
+        public abstract double[] ShowDayChange(double baseStk, double percChange, int sign);
+        public abstract double[] ShowTtlChange(double baseStk, double[] dayChange);
         public abstract double CurrStockVal(double stockVal);
     }
     public class Google : StkPortfolio
     {
-        public override int RandVal(double minPerc, double maxPerc)
+        double googleStk = 250.00;
+        StkPortfolio google;
+
+        public override double RandStkVal(double minPerc, double maxPerc)
         {
-            throw new NotImplementedException();
+            Random random = new Random();
+            double percent = random.NextDouble() * (maxPerc - minPerc) + minPerc;
+            percent = Math.Round(percent, 2);
+            return percent;
         }
-        public override double ShowDayChange(double dayChange, double percChange)
+        public override int RandVal(int minPerc, int maxPerc)
         {
-            throw new NotImplementedException();
+            int sign;
+            Random random = new Random();
+            int signPercent = random.Next() * (maxPerc - minPerc) + minPerc;
+            if (signPercent == 0)
+            {
+                sign = -1;
+            }
+            else
+                sign = 1;
+            return sign;
         }
-        public override double ShowTtlChange(double ttlChange, double dayChange)
+        public override double[] ShowDayChange(double baseStk, double percChange, int sign)
         {
-            throw new NotImplementedException();
+            google = new Google();
+            double amntChange;
+            double dayChange;
+            baseStk = google.CurrStockVal(googleStk);
+            percChange = google.RandStkVal(.01, .05);
+            sign = google.RandVal(0, 1);
+            amntChange = baseStk * percChange * sign;
+            dayChange = Math.Round(amntChange + baseStk, 2);
+            double[] values = new double[2] { amntChange, dayChange };
+            return values;
+        }
+        public override double[] ShowTtlChange(double baseStk, double[] dayChange)
+        {
+            google = new Google();
+            baseStk = google.CurrStockVal(googleStk);
+            dayChange = google.ShowDayChange(google.CurrStockVal(googleStk), google.RandStkVal(.01, .05), google.RandVal(0, 1));
+            double currVal = baseStk + dayChange[0];
+            double ttlChange = currVal - baseStk;
+            double ttlStk = baseStk + ttlChange;
+            double[] values = new double[2] { ttlChange, ttlStk };
+            return values;
         }
         public override double CurrStockVal(double currVal)
         {
-            throw new NotImplementedException();
+            currVal = googleStk;
+            return currVal;
         }
     }
-    public class Gold : StkPortfolio
-    {
-        public override int RandVal(double minPerc, double maxPerc)
-        {
-            throw new NotImplementedException();
-        }
-        public override double ShowDayChange(double dayChange, double percChange)
-        {
-            throw new NotImplementedException();
-        }
-        public override double ShowTtlChange(double ttlChange, double dayChange)
-        {
-            throw new NotImplementedException();
-        }
-        public override double CurrStockVal(double currVal)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //public class Gold : StkPortfolio
+    //{
+    //    public override double RandVal(double minPerc, double maxPerc)
+    //    {
+    //        Random random = new Random();
+    //        return random.NextDouble() * (maxPerc - minPerc) + minPerc;
+
+    //    }
+    //    public override double ShowDayChange(double dayChange, double percChange)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //    public override double ShowTtlChange(double ttlChange, double origVal)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //    public override double CurrStockVal(double currVal)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
--- UnitTestSampleFIP/UnitTests.cs
+++ UnitTestSampleFIP/UnitTests.cs
@@ -15,11 +15,11 @@ namespace UnitTestSampleFIP
             clientAcct.AcctBalance(1000);
             clientAcct.GetSalary(10000);
             clientAcct.PercInvest(10000, .10);
-            Assert.AreEqual(1000, clientAcct.AcctBalance(1000));
+            Assert.AreEqual(1000,  clientAcct.AcctBalance(1000));
             Assert.AreEqual(10000, clientAcct.GetSalary(10000));
-            Assert.AreEqual(1000, clientAcct.PercInvest(10000, .10));
-            Assert.AreEqual(1000, clientAcct.Perc401k(10000, .10));
-            Assert.AreEqual(1000, clientAcct.PercLISA(10000, .10));
+            Assert.AreEqual(1000,  clientAcct.PercInvest(10000, .10));
+            Assert.AreEqual(1000,  clientAcct.Perc401k(10000, .10));
+            Assert.AreEqual(1000,  clientAcct.PercLISA(10000, .10));
 
         }
         [TestMethod]
@@ -66,14 +66,71 @@ namespace UnitTestSampleFIP
         {
             StkPortfolio stkPortfolio = new Google();
 
-            stkPortfolio.CurrStockVal(150.00);
-            stkPortfolio.RandVal(.01, .05);
-            stkPortfolio.ShowDayChange(157.5, .05);
-            stkPortfolio.ShowTtlChange(162.23, 157.5);
-            Assert.AreEqual(150.00, stkPortfolio.CurrStockVal(150.00));
-            Assert.AreEqual(.03, stkPortfolio.RandVal(.01, .05));
-            Assert.AreEqual(24.45, stkPortfolio.ShowDayChange(24.45, .05));
-            Assert.AreEqual(150, stkPortfolio.ShowTtlChange(162.23, 157.5));
+            stkPortfolio.CurrStockVal(250.00);
+            stkPortfolio.RandStkVal(.01, .05);
+            stkPortfolio.ShowDayChange(250.00, .05, 1);
+            stkPortfolio.ShowTtlChange(162.23, stkPortfolio.ShowDayChange(250.00, .05, 1));
+            Assert.AreEqual(250.00, stkPortfolio.CurrStockVal(250.00));
+            Assert.IsTrue(stkPortfolio.RandStkVal(.01, .05) <= .05);
+            Assert.IsTrue(stkPortfolio.RandStkVal(.01, .05) >= .01);
+            double change = stkPortfolio.ShowDayChange(250.00, .05, 1)[0];
+            double value = stkPortfolio.ShowDayChange(250.00, .05, 1)[1];
+            Assert.IsTrue(change <= 12.50);
+            Assert.IsTrue(change >= -12.50);
+            Assert.IsTrue(value <= 262.50);
+            Assert.IsTrue(value >= 238.50);
+            double ttlChange = stkPortfolio.ShowTtlChange(250.00, stkPortfolio.ShowDayChange(250.00, .05, 1))[0];
+            double ttlValue = stkPortfolio.ShowTtlChange(250.00, stkPortfolio.ShowDayChange(250.00, .05, 1))[1];
+            Assert.IsTrue(ttlChange <= 12.50);
+            Assert.IsTrue(ttlChange >= -12.50);
+            Assert.IsTrue(ttlValue <= 262.50);
+            Assert.IsTrue(ttlValue >= 238.50);
+        }
+        [TestMethod]
+        public void TestCurrVal()
+        {
+            StkPortfolio stkPortfolio = new Google();
+            Assert.AreEqual(250.00, stkPortfolio.CurrStockVal(250.00));
+        }
+        [TestMethod]
+        public void TestRandStkVal()
+        {
+            StkPortfolio stkPortfolio = new Google();
+            stkPortfolio.RandStkVal(.01, .05);
+            Assert.IsTrue(stkPortfolio.RandStkVal(.01, .05) <= .05);
+            Assert.IsTrue(stkPortfolio.RandStkVal(.01, .05) >= .01);
+        }
+        [TestMethod]
+        public void TestRandVal()
+        {
+            StkPortfolio stkPortfolio = new Google();
+            stkPortfolio.RandVal(0, 1);
+            Assert.IsTrue(stkPortfolio.RandVal(0, 1) <= 1);
+            Assert.IsTrue(stkPortfolio.RandVal(0, 1) >= 0);
+        }
+        [TestMethod]
+        public void TestDayChange()
+        {
+            StkPortfolio stkPortfolio = new Google();
+            double change = stkPortfolio.ShowDayChange(250.00, .05, 1)[0];
+            double value = stkPortfolio.ShowDayChange(250.00, .05, 1)[1];
+            Assert.IsTrue(change <= 12.50);
+            Assert.IsTrue(change >= -12.50);
+            Assert.IsTrue(value <= 262.50);
+            Assert.IsTrue(value >= 238.50);
+        }
+        [TestMethod]
+        public void TestShowTtlChange()
+        {
+           
+            StkPortfolio stkPortfolio = new Google();
+            double ttlChange = stkPortfolio.ShowTtlChange(250.00, stkPortfolio.ShowDayChange(250.00, .05, 1))[0];
+            double ttlValue = stkPortfolio.ShowTtlChange(250.00, stkPortfolio.ShowDayChange(250.00, .05, 1))[1];
+            Assert.IsTrue(ttlChange <= 12.50);
+            Assert.IsTrue(ttlChange >= -12.50);
+            Assert.IsTrue(ttlValue <= 262.50);
+            Assert.IsTrue(ttlValue >= 238.50);
+
         }
     }
 }
